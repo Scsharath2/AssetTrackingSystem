8485aea8e29f7fdf45e8f9060849fd60c0421961
Sharath chandra Gajjela <SGAJJELA@amdocs.com>
2025-03-07 21:19:05 +0530

    UI changes

--- asset-management-ui/src/components/Sidebar.js
+++ asset-management-ui/src/components/Sidebar.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { Drawer, List, ListItem, ListItemText } from "@mui/material";
+import React, { useState } from "react";
+import { Drawer, List, ListItem, ListItemText, Typography, Collapse } from "@mui/material";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const [openServiceNowSync, setOpenServiceNowSync] = useState(false);
+
+  const handleServiceNowClick = () => {
+    setOpenServiceNowSync(!openServiceNowSync);
+  };
+
   return (
     <Drawer variant="permanent" sx={{ width: 240 }}>
       <List>
@@ -15,12 +22,34 @@ const Sidebar = () => {
         <ListItem button component={Link} to="/scheduling">
           <ListItemText primary="Scheduling" />
         </ListItem>
-        <ListItem button component={Link} to="/mapping">
-          <ListItemText primary="Mapping" />
+
+
+        {/* ServiceNow Sync - Clickable Header */}
+        <ListItem button onClick={handleServiceNowClick}>
+          <ListItemText primary="ServiceNow Sync" />
+          {openServiceNowSync ? <ExpandLess /> : <ExpandMore />}
         </ListItem>
-        <ListItem button component={Link} to="/assets">
-          <ListItemText primary="Asset Management" />
-        </ListItem>
+
+        {/* Collapsible ServiceNow Sync Pages */}
+        <Collapse in={openServiceNowSync} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button component={Link} to="/servicenow/assets" sx={{ pl: 4 }}>
+              <ListItemText primary="Assets" />
+            </ListItem>
+            <ListItem button component={Link} to="/servicenow/employees" sx={{ pl: 4 }}>
+              <ListItemText primary="Employees" />
+            </ListItem>
+            <ListItem button component={Link} to="/servicenow/locations" sx={{ pl: 4 }}>
+              <ListItemText primary="Locations" />
+            </ListItem>
+            <ListItem button component={Link} to="/servicenow/models" sx={{ pl: 4 }}>
+              <ListItemText primary="Models" />
+            </ListItem>
+            <ListItem button component={Link} to="/servicenow/stock" sx={{ pl: 4 }}>
+              <ListItemText primary="Stock" />
+            </ListItem>
+          </List>
+        </Collapse>
       </List>
     </Drawer>
   );
